adc92e7a9622a830c57eeb2f121fde144f253da8
Danilo Petkovic <petkovicdanilo97@gmail.com>
2018-10-25 00:28:56 +0200

    Added structure to server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+// TODO Veljko
+// konekcija ka bazi
+
 app.listen(8081, () => {
     console.log('Server listening on port 8081');
 });
