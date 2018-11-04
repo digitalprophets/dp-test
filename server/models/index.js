5ef39233c053ab384ea5d401a16bda8bc09b7f15
Danilo Petkovic <petkovicdanilo97@gmail.com>
2018-11-04 16:12:45 +0100

    Automatic creation of the database

--- server/models/index.js
+++ server/models/index.js
@@ -8,49 +8,78 @@ const basename = path.basename(module.filename);
 // object to store all models and connection object
 const db = {};
 
-// connect to database
-const sequelize = new Sequelize('dptest', 'root', '', {
+// connect to database server
+const sq = new Sequelize('', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql',
-  
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
+            dialect: 'mysql',
+        
+            pool: {
+            max: 1,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+            },
 });
 
-// check if connection is successfull
-sequelize
+// test connection to database server
+sq
     .authenticate()
     .then( () => {
-        console.log('Connected to database');
+
+        // create database if not exits
+        sq.query('CREATE DATABASE IF NOT EXISTS dptest')
+        .then( () => {
+
+            sq.close();
+    
+            // connect to database
+            const sequelize = new Sequelize('dptest', 'root', '', {
+                host: 'localhost',
+                dialect: 'mysql',
+            
+                pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000,
+                idle: 10000
+                },
+            });
+    
+            // check if connection is successfull
+            sequelize
+                .authenticate()
+                .then( () => {
+                    console.log('Connected to database');
+                })
+                .catch( err => {
+                    console.log('Failed to connect to the database', err);
+                });
+    
+            // load all models into db object
+            fs
+                .readdirSync(__dirname)
+                .filter(file => {
+                    return (file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js');
+                })
+                .forEach(file => {
+                    const model = sequelize['import'](path.join(__dirname, file));
+                    model.sync();
+                    db[model.name] = model;
+                });
+    
+            // add foreign key constraints
+            Object.keys(db).forEach(modelName => {
+                if (db[modelName].associate) {
+                    db[modelName].associate(db);
+                }
+            });
+    
+            db.sequelize = sequelize;
+            db.Sequelize = Sequelize;
+    
+            module.exports = db;
+        })
     })
     .catch( err => {
-        console.log('Failed to connect to the database', err);
+        console.log('Unable to create database', err);
     });
-
-// load all models into db object
-fs
-    .readdirSync(__dirname)
-    .filter(file => {
-        return (file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js');
-    })
-    .forEach(file => {
-        const model = sequelize['import'](path.join(__dirname, file));
-        model.sync();
-        db[model.name] = model;
-    });
-
-// add foreign key constraints
-Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
